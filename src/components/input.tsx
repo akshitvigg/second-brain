0c757d444c663f2cfe959f4f915f7b7f7c004751
Akshit Vig <akshitvig48@gmail.com>
2024-12-01 23:42:00 +0530

    res

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -5,15 +5,24 @@ interface inputProps {
   modal ? : boolean
 }
 
-export const InputComp = ({ placeholder, reference, width,modal }: inputProps) => {
+export const InputComp = ({ placeholder, reference,modal }: inputProps) => {
   return (
     <div className="mt-4 ">
       <input
-        className={` outline-none sm:px-12 sm:py-2 px-7 py-2 border-slate-400 border rounded-md  `}
+        className={` outline-none sm:px-12 sm:py-2  border-slate-400 border rounded-md 
+             ${modal && "sm:py-3 py-3 w-[335px]"}  `}
         type="text"
         placeholder={placeholder}
         ref={reference}
       />
     </div>
   );
+
+//   className={` outline-none sm:px-12 sm:py-2 px-6 py-2 border-slate-400 border rounded-md ${
+//     width && "w-[335px]"
+//   } ${modal && "sm:py-3 py-3"} `}
+//   type="text"
+//   placeholder={placeholder}
+//   ref={reference}
+// />
 };
