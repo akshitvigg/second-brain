c0ed50c53669a19c0a4d83427f0927d8bbff999f
Akshit Vig <akshitvig48@gmail.com>
2024-12-01 23:32:23 +0530

    res

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -9,7 +9,7 @@ export const InputComp = ({ placeholder, reference, width,modal }: inputProps) =
   return (
     <div className="mt-4 ">
       <input
-        className={` outline-none sm:px-12 sm:py-2 px-6 py-2 border-slate-400 border rounded-md ${
+        className={` outline-none sm:px-12 sm:py-2 !px-6 py-2 border-slate-400 border rounded-md ${
           width && "w-[335px]"
         } ${modal && "sm:py-3 py-3"} `}
         type="text"
