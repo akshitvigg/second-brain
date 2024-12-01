6cb3e36328113f5d6d26850a783b661ddadd7522
Akshit Vig <akshitvig48@gmail.com>
2024-12-02 00:32:54 +0530

    sds

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -9,7 +9,7 @@ export const InputComp = ({ placeholder, reference }: inputProps) => {
   return (
     <div className="mt-4 ">
       <input
-        className={` sm:py-3 sm:px-[73px] py-3 px-9  border-slate-400 border rounded-md 
+        className={` sm:py-3 sm:px-[73px] py-3 px-6  border-slate-400 border rounded-md 
        `}
         type="text"
         placeholder={placeholder}
