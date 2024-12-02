20e3bc77e62c471049206478da608f47fc4e8840
Akshit Vig <akshitvig48@gmail.com>
2024-12-02 14:38:37 +0530

    commit

--- src/components/authinput.tsx
+++ src/components/authinput.tsx
@@ -8,7 +8,7 @@ interface inputProps {
     return (
       <div className="mt-4 ">
         <input
-          className={` outline-none sm:px-12 sm:py-2 px-5 py-2  border-slate-400 border rounded-md `}
+          className={`outline-none py-2 sm:w-72 w-64 pl-2  border-slate-400 border rounded-md  `}
                
           type="text"
           placeholder={placeholder}
