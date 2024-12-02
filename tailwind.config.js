c6849e645b032232489449c01c28f85815071350
Akshit Vig <akshitvig48@gmail.com>
2024-12-02 14:32:51 +0530

    poppins

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ export default {
         },
       },
       fontFamily: {
-        appleRound: ['CustomAppleFont'],
+        poppins : ['Poppins'],
       },
       height: {
         "custom-h": "460px",
