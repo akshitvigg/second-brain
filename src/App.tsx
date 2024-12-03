744ffb5283cfc3ed859d8021aa183ec40b330632
Akshit Vig <akshitvig48@gmail.com>
2024-12-03 18:24:32 +0530

    landingpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Signup } from "./components/signup";
@@ -7,10 +7,31 @@ import { CreateContentModal } from "./components/CreateContentModal";
 import { ShareBrain } from "./components/ShareBrain";
 import { SideBar } from "./components/sidebar";
 import SharedContent from "./components/sharesidebar";
+import { LandingPage } from "./components/landingPage";
+import Features from "./components/features";
+import HowItWorks from "./components/howitworks";
+import { Footer } from "./components/footer";
 
 function App() {
   const [modalOpen, setModalOpen] = useState(false);
   const [sharemodalOpen, setshareModalOpen] = useState(false);
+
+  const Homecontent = () => {
+    const featuresRef = useRef(null);
+    const howItWorksRef = useRef(null);
+    return (
+      <>
+        <LandingPage />
+        <div ref={featuresRef} id="features">
+          <Features />
+        </div>
+        <div ref={howItWorksRef} id="how-it-works">
+          <HowItWorks />
+        </div>
+        <Footer />
+      </>
+    );
+  };
 
   return (
     <>
@@ -34,7 +55,8 @@ function App() {
               />
             }
           />
-          <Route path="/" element={<Signup />} />
+          <Route path="/" element={<Homecontent />} />
+          <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/shared/:sharelink" element={<SharedContent />} />
         </Routes>
