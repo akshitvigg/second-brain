b9d0c85ea16f087989f2f789b1c0e8298ae050ca
Akshit Vig <akshitvig48@gmail.com>
2024-12-03 22:32:05 +0530

    eyeoffeyeon

--- src/components/authinput.tsx
+++ src/components/authinput.tsx
@@ -1,20 +1,19 @@
 interface inputProps {
-    placeholder: string;
-    reference?: any;
-    width?: string;
-  }
-  
-  export const AuthInputcomp = ({ placeholder, reference}: inputProps) => {
-    return (
-      <div className="mt-4 ">
-        <input
-          className={`outline-none py-2 sm:w-72 w-64 pl-2  border-slate-400 border rounded-md  `}
-               
-          type="text"
-          placeholder={placeholder}
-          ref={reference}
-        />
-      </div>
-    );
+  placeholder: string;
+  reference?: any;
+
+  type?: string;
+}
+
+export const AuthInputcomp = ({ placeholder, reference, type }: inputProps) => {
+  return (
+    <div className="mt-4 ">
+      <input
+        className={`outline-none py-2 sm:w-72 w-64 pl-2  border-slate-400 border rounded-md  `}
+        type={type}
+        placeholder={placeholder}
+        ref={reference}
+      />
+    </div>
+  );
 };
-  
