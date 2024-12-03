9189c3f3541a83eaec4c8c38098c9e4b57d899ca
Akshit Vig <akshitvig48@gmail.com>
2024-12-03 18:24:19 +0530

    landingpage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode : "selector",
+  darkMode: "selector",
   theme: {
     extend: {
       colors: {
@@ -12,7 +12,7 @@ export default {
         },
       },
       fontFamily: {
-        poppins : ['Poppins'],
+        poppins: ["Poppins"],
       },
       height: {
         "custom-h": "460px",
